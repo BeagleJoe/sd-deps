1041c5d24a93ef75122355f16c140ae002d34a7b
BeagleJoe <BeagleJoe13@gmail.com>
2025-11-29 14:59:42 -0600

    Update vsg to 1.1.12

--- thirdpartydefinitions.cmake
+++ thirdpartydefinitions.cmake
@@ -256,11 +256,11 @@ set(OPENCL_URL https://github.com/KhronosGroup/OpenCL-SDK/releases/download/${OP
 set(OPENCL_HASH SHA256=29dc06da27d264d8e8592d0ba3e5d3d3ecefb3394f808e67b70ad3124b291a38)
 
 # VulkanSceneGraph
-set(VSG_VERSION 1.1.11)
+set(VSG_VERSION 1.1.12)
 set(VSG_PROJECT VulkanSceneGraph-${VSG_VERSION})
 set(VSG_FILE ${VSG_PROJECT}.tar.gz)
 set(VSG_URL https://github.com/vsg-dev/VulkanSceneGraph/archive/refs/tags/v${VSG_VERSION}.tar.gz)
-set(VSG_HASH SHA256=11d2ecaea0f10c717ea90fdd494a4e899d59b847b68dc1a47d1370de89f095e5)
+set(VSG_HASH SHA256=bd9c58f615d44a4de684fc0c97acc010a548a6714a64c98c59ee2b86f3e66e3d)
 
 # TinyGLTF 
 set(TINYGLTF_VERSION 2.9.7)
